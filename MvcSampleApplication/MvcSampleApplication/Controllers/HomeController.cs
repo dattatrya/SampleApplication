47d2bc36411f9ffe7adcbddb2e9c4a39b159f922
dattatrya <datta321@gmail.com>
2015-09-23 12:32:49 +0530

    Added Home Controller , View, Model

---
+++ MvcSampleApplication/MvcSampleApplication/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcSampleApplication.Models;
+
+namespace MvcSampleApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Home()
+        {
+            Home HM = new Home();
+            HM.id = 100;
+            HM.name = "Dattatrya Yerande";
+            ViewData["ID"] = HM.id;
+            ViewData["Name"] = HM.name;
+            return View(HM);
+        }
+    }
+}
---
+++ MvcSampleApplication/MvcSampleApplication/Models/Home.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcSampleApplication.Models
+{
+    public class Home
+    {
+        public int id { set; get; }
+        public string name {set; get;}
+    }
+}
